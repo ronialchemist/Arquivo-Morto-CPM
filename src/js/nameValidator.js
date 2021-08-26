47702c3b6bf83111a49c3febc3b3544e5a70e7ef
ronialchemist <ronialchemist@gmail.com>
2021-08-25 23:34:22 -0300

    dependency for form validation added

---
+++ src/js/nameValidator.js
@@ -0,0 +1,20 @@
+const test = (name) => {
+	const schema = joi.object({
+		name: joi
+					.string()
+					.required()
+					.min(2)
+					.max(40)
+					.pattern(/^[^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/)
+					.messages({
+						'string.empty': `O campo "Nome" não pode estar vazio`,
+						'string.min': `O nome deve ter no mínimo 2 caracteres`,
+						'string.max': `O nome deve ter no máximo 40 caracteres`,
+						'string.pattern.base': `O nome não é um nome válido`
+					}),
+	});
+
+	const { error } = schema.validate({ name });
+
+	return error.message;
+}
